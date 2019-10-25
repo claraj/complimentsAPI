03fd022595d51eb1abdbe1b95ef91d8c2c44a760
clara <clara@minneapolis.edu>
2019-10-25 08:48:05 -0500

    more compliments

--- src/main/java/com/example/hello/ComplimentController.java
+++ src/main/java/com/example/hello/ComplimentController.java
@@ -15,15 +15,32 @@ import java.util.Random;
 public class ComplimentController {
 
     private Compliment[] compliments = {
-            new Compliment("You look nice today"),
+            new Compliment("You look nice today!"),
             new Compliment("Your code is awesome!"),
             new Compliment("I'm so happy to see you!"),
-            new Compliment("I hope you have a great day!"),
+            new Compliment("You deserve to have a great day!"),
             new Compliment("Super fun to code with you!"),
             new Compliment("You are a smart cookie!"),
             new Compliment("On a scale of 1 to 10, you are an 11!"),
             new Compliment("You are a ray of sunshine!"),
-            new Compliment("Thanks for being you!")  
+            new Compliment("Thanks for being you!"),
+            new Compliment("You have great style!"),
+            new Compliment("You light up the room!"),
+            new Compliment("You have the best sense of humor!"),
+            new Compliment("You are all that and more!"),
+            new Compliment("You are even more beautiful on the inside than the outside!"),
+            new Compliment("You are an inspiration!"),
+            new Compliment("You make a difference to the world!"),
+            new Compliment("You are a ray of sunshine!"),
+            new Compliment("You are the best thing since sliced bread!"),
+            new Compliment("You are so creative!"),
+            new Compliment("You have the best ideas!"),
+            new Compliment("You rock!"),
+            new Compliment("You can accomplish anything!"),
+            new Compliment("You are breathtaking!"),
+            new Compliment("You are inspiring!"),
+            new Compliment("You are so smart!"),
+            
     };
 
     private Random rnd = new Random();
